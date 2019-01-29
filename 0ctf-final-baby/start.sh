eba7245f3bc34673cb9f200d1889743bd0fa2d2b
ret2p4nda <p4nda_lbh@bupt.edu.cn>
2019-01-28 22:14:12 -0800

    add 0ctf 2018 final baby

---
+++ 0ctf-final-baby/start.sh
@@ -0,0 +1,9 @@
+qemu-system-x86_64 \
+-m 256M -smp 2,cores=2,threads=1  \
+-kernel ./vmlinuz-4.15.0-22-generic \
+-initrd  ./core.cpio \
+-append "root=/dev/ram rw console=ttyS0 oops=panic panic=1 quiet" \
+-cpu qemu64 \
+-netdev user,id=t0, -device e1000,netdev=t0,id=nic0 \
+-gdb tcp::1234 -S \
+-nographic  -enable-kvm  \
